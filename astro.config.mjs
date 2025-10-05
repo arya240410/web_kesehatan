8ef174f78a680a3b2963d7b2405cb8c19a319675
arya240410 <bocilgaje24@gmail.com>
2025-10-05 19:29:45 +0700

    Refactor Bootstrap imports to use CDN links for improved performance and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,10 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   base: '/web_kesehatan/',
+  vite: {
+    build: {
+      assetsInlineLimit: 0,
+      copyPublicDir: true
+    }
+  }
 });
